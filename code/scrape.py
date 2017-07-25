2123ee861d7d46f4c01fdbd6d1e0a3e56a0f2f88
immanuelpotter <manny.potter@capgemini.com>
2017-07-25 10:36:57 +0000

    Finished script and added results from script

--- code/scrape.py
+++ code/scrape.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -13,11 +14,15 @@ table = soup.find('tbody', attrs={'class': 'stripe'})
 
 list_of_rows = []
 #Find table rows in table, then table data tags and structure them
-for row in table.findAll('tr'):
+for row in table.findAll('tr')[1:]:
 	list_of_cells = []
 	for cell in row.findAll('td'):
-		text = cell.text.replace('&nbsp;','')
+		text = cell.text.replace('&nbsp;','').encode('utf-8')
 		list_of_cells.append(text)
 	list_of_rows.append(list_of_cells)
 
-print list_of_rows
+#Save our results to a .csv file
+outfile = open("./inmates.csv",'wb')
+writer = csv.writer(outfile)
+writer.writerow(["Last", "First", "Middle", "Gender", "Race", "Age", "City", "State"])
+writer.writerows(list_of_rows)
