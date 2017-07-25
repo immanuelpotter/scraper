ebecdcd6c421e3d695ece2a3e79b427dbf59efc6
immanuelpotter <manny.potter@capgemini.com>
2017-07-25 10:16:32 +0000

    List of list structure added

--- code/scrape.py
+++ code/scrape.py
@@ -1,9 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+#Set up URL to send request and get response from
 url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
 response = requests.get(url)
 html = response.content
 
+
 soup = BeautifulSoup(html)
-print soup.prettify()
+#Find our table body elements
+table = soup.find('tbody', attrs={'class': 'stripe'})
+
+list_of_rows = []
+#Find table rows in table, then table data tags and structure them
+for row in table.findAll('tr'):
+	list_of_cells = []
+	for cell in row.findAll('td'):
+		text = cell.text.replace('&nbsp;','')
+		list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+print list_of_rows
